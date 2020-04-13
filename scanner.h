9fe6d111e67b333e5ba738548ac8526ef13f00af
hagarhaytham <hagar.bahy97@eng-st.cu.edu.eg>
2020-04-13 05:12:20 +0200

    adding some keywords

--- scanner.h
+++ scanner.h
@@ -5,4 +5,14 @@
 #define CHAR 5
 #define STRING 6
 #define IDENTIFIER 7
+#define IF 8
+#define ELSE 9
+#define WHILE 10
+#define DO 11
+#define FOR 12
+#define SWICH 13
+#define CASE 14
+#define DEFAULT 15 
+#define PRINT 16
+#define SEMICOLON 17
 #define OTHER 8
